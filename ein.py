569f42e13ece7fcc5017bf3e92016668b5af685e
Andreas Roehler <andreas.roehler@online.de>
2025-07-13 14:52:51 +0200

    ein.py added
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

---
+++ ein.py
@@ -0,0 +1,10 @@
+def describe_point(point): match point: case (0, 0): return
+"Origin" case (0, y): return f"Y-axis, y={y}" case (x, 0): return
+f"X-axis, x={x}" case (x, y): return f"X={x}, Y={y}" case _: return
+"Not a point"
+
+print(describe_point((0, 0))) # Output: Origin
+print(describe_point((0, 5))) # Output: Y-axis, y=5
+print(describe_point((3, 0))) # Output: X-axis, x=3
+print(describe_point((2, 3))) # Output: X=2, Y=3
+print(describe_point("not a point")) # Output: Not a point 
